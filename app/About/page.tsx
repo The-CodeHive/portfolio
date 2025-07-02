1552a6ddb0b66230cc2417f643889b2652ca4396
Jagdeep Singh <jagdeep.singh12a@gmail.com>
2025-07-02 17:39:06 +0530

    updated links

---
+++ app/About/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const MoreAboutMe = () => {
+  return (
+    <div className='satoshi'>More About Me</div>
+  )
+}
+
+export default MoreAboutMe
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -18,46 +18,46 @@ const Footer = () => {
           <nav className="footer-nav">
             <ul>
               <li><a href="#">Home</a></li>
-              <li><a href="#">Projects</a></li>
-              <li><a href="#">About</a></li>
-            </ul> 
+              <li><a href="/Projects">Projects</a></li>
+              <li><a href="/About">About</a></li>
+            </ul>
           </nav>
         </div>
 
         {/* Right Column - Emails & Socials */}
         <div className="footer-column right">
           <div className="contact-links-group">
-            <a href="mailto:jagdeep.singh12a@gmail.com" className="contact-link">
+            <a href="mailto:jagdeep.singh12a@gmail.com" target='_blank' className="contact-link">
                Work Email 
               <ArrowUpRight className="contact-link-icon" />
             </a>
-            <a href="mailto:jagdeep.singh12awork@gmail.com" className="contact-link">
+            <a href="mailto:jagdeep.singh12awork@gmail.com" target='_blank' className="contact-link">
                Personal Email 
               <ArrowUpRight className="contact-link-icon" />
             </a>
           </div>
 
           <div className="contact-links-group">
-            <a href="#" className="contact-link">
+            <a href="https://www.linkedin.com/in/jagdeep-singh-812986249/" target='_blank' className="contact-link">
                LinkedIn 
               <ArrowUpRight className="contact-link-icon" />
             </a>
-            <a href="#" className="contact-link">
+            <a href="https://github.com/jagdep-singh" target='_blank' className="contact-link">
                Github 
               <ArrowUpRight className="contact-link-icon" />
             </a>
           </div>
 
           <div className="contact-links-group">
-            <a href="#" className="contact-link">
+            <a href="https://www.instagram.com/jazz_.deep/" target='_blank' className="contact-link">
                Instagram 
               <ArrowUpRight className="contact-link-icon" />
             </a>
-            <a href="#" className="contact-link">
+            <a href="https://www.threads.com/@jazz_.deep" target='_blank' className="contact-link">
                Threads 
               <ArrowUpRight className="contact-link-icon" />
             </a>
-            <a href="#" className="contact-link">
+            <a href="https://twitter.com/" target='_blank' className="contact-link">
                X (Twitter) 
               <ArrowUpRight className="contact-link-icon" />
             </a>
